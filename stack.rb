1e9c708b522405f641c023efaf232bc2e3f4cfe0
Torey Hickman <torey@toreyhickman.com>
2015-10-07 14:28:27 -0500

    Add comments on stack methods

--- stack.rb
+++ stack.rb
@@ -5,6 +5,9 @@ class Stack
     @store = Array.new
   end
 
+
+  # Add an element
+  # to the top of the stack.
   def push(x)
     store.push(x)
 
@@ -13,15 +16,24 @@ class Stack
     return self
   end
 
+
+  # Remove an element
+  # from the stack.
   def pop
     raise(UnderflowError, "There is no element to remove from the stack") if empty?
     store.pop
   end
 
+
+  # Show the element on the top of the stack,
+  # but don't remove it.
   def peek
     store.last
   end
 
+
+  # Report whether or not ther are
+  # any elements in the stack.
   def empty?
     store.empty?
   end
